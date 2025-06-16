1277332b40435deebd8715f2ef99bca858b71132
Jere Helenius <Jere.helenius2@gmail.com>
2025-06-16 13:13:21 +0300

    First commit

---
+++ Components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar(){
+    return(
+        <header className="navhead">
+            <nav className="navmain">
+                <img className="nav-img" src="react-logo.png" alt="Logo" />
+                <span className="nav-text">ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
---
+++ app.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./Components/Navbar"
+import MainCont from "./Components/Main"
+
+
+export default function App(){
+    return(
+<>
+    <NavBar />
+    <MainCont /> 
+</>  
+    
+    )
+}
